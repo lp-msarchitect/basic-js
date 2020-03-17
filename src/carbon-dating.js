fd497ee0329cc29ae8b88383b3df8283a585c08e
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-17 16:20:09 +0300

    refac: remove comments in carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,18 +2,6 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  /*if (
-    typeof sampleActivity !== "string" ||
-    isNaN(+sampleActivity) ||
-    +sampleActivity < 0
-  ) {
-    return false;
-  }
-  let age = Math.ceil(
-    Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) /
-      (0.693 / HALF_LIFE_PERIOD)
-  );
-  return age;*/
   if (
     typeof sampleActivity !== "string" ||
     isNaN(parseFloat(sampleActivity)) ||
