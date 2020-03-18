207e73c1a08e8c989491663c0e8438a63b18701f
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-18 21:02:54 +0300

    feat: Implement extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,32 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+  options.separator = options.separator || "+";
+  options.additionSeparator = options.additionSeparator || "|";
+  options.repeatTimes = options.repeatTimes || 1;
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  if (str === null) {
+    str = "null";
+  }
+
+  if (options.addition === null) {
+    options.addition = "null";
+  }
+
+  let additionRepeatArr = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionRepeatArr.push(options.addition);
+  }
+
+  let additionRepeatStr = additionRepeatArr.join(options.additionSeparator);
+
+  let resultArr = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    resultArr.push(str + additionRepeatStr);
+  }
+
+  let resultStr = resultArr.join(options.separator);
+
+  return resultStr;
 };
-  
