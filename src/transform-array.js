f0a335096bb85c796eeb6c2a7af263ee379128de
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-18 23:43:29 +0300

    feat: implement transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,36 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case "--discard-prev":
+        if (resultArr.length > 0) {
+          resultArr.pop();
+        }
+        break;
+      case "--double-next":
+        if (i < arr.length - 1) {
+          resultArr.push(arr[i + 1]);
+        }
+
+        break;
+      case "--double-prev":
+        if (i > 0) {
+          resultArr.push(arr[i - 1]);
+        }
+        break;
+      default:
+        resultArr.push(arr[i]);
+        break;
+    }
+  }
+
+  return resultArr;
 };
