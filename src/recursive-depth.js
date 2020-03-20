44ac64ba9a9e2b241d8025a07fbdc039d8729d40
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-20 17:54:28 +0300

    feast: Implement recursive depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,19 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  calculateDepth(arr) {
+    let isPlate = true;
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        isPlate = false;
+        for (let j = 0; j < arr[i].length; j++) {
+          newArr.push(arr[i][j]);
+        }
+      }
     }
+    if (isPlate) {
+      return 1;
+    } else {
+      return 1 + this.calculateDepth(newArr);
+    }
+  }
 };
