bfd3978e642b0f620978a02f1eddadb8479dcd52
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-22 14:16:05 +0300

    Implement hanoi-tower and vigenere cipher solutions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,86 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  encrypt(message = null, key = null) {
+    if (message === null || key === null) {
+      throw Error;
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encryptMsg = "";
+    let encryptStep = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message.charAt(i).toUpperCase();
+      const keyChar = key.charAt(encryptStep % key.length).toUpperCase();
+      if (this.getCharCode(char) === -1) {
+        encryptMsg += char;
+      } else {
+        const encryptChar = this.getCharFromCode(
+          this.getCharCode(char) + this.getCharCode(keyChar)
+        );
+        encryptStep++;
+        encryptMsg += encryptChar;
+      }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    if (this.isDirect) {
+      return encryptMsg;
+    } else {
+      return encryptMsg
+        .split("")
+        .reverse()
+        .join("");
     }
+  }
+
+  decrypt(message = null, key = null) {
+    if (message === null || key === null) {
+      throw Error;
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let decryptMsg = "";
+    let decryptStep = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message.charAt(i).toUpperCase();
+      const keyChar = key.charAt(decryptStep % key.length).toUpperCase();
+      if (this.getCharCode(char) === -1) {
+        decryptMsg += char;
+      } else {
+        let decryptCharCode =
+          this.getCharCode(char) - this.getCharCode(keyChar);
+        if (decryptCharCode < 0) {
+          decryptCharCode = this.ALPHABET.length + decryptCharCode;
+        }
+        const decryptChar = this.getCharFromCode(decryptCharCode);
+        decryptStep++;
+        decryptMsg += decryptChar;
+      }
+    }
+
+    if (this.isDirect) {
+      return decryptMsg;
+    } else {
+      return decryptMsg
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+
+  getCharCode(char) {
+    return this.ALPHABET.indexOf(char);
+  }
+
+  getCharFromCode(code) {
+    code = code % this.ALPHABET.length;
+    return this.ALPHABET.charAt(code);
+  }
 }
 
 module.exports = VigenereCipheringMachine;
